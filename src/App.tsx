a5d586342e536ac63cdb9e46c8971c2532b18342
Oluwasegun <olufasco1@gmail.com>
2026-08-11 10:22:42 +0100

    Handling Events

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
-import CardList from "./Components/CardList/CardList"
 import './App.css';
+import CardList from "./Components/CardList/CardList"
+import Search from "./Components/Search/Search";
 
 function App() {
   return (
     <div className="App">
+      <Search />
       <CardList />
     </div>
   );
