d48604e8c63a180a2afe764ff9e8b42729a591bf
Oluwasegun <olufasco1@gmail.com>
2026-08-13 23:38:35 +0100

    Adjust Api.tsx

--- src/api.tsx
+++ src/api.tsx
@@ -6,16 +6,20 @@ interface SearchResponse {
 }
 
 export const searchCompanies = async (query: string) =>{
-    try {
+    try 
+    {
         const data = await axios.get<SearchResponse>(`https://financialmodelingprep.com/stable/search-name?query=${query}&apikey=${process.env.REACT_APP_API_KEY}`);
         return data;
     }
-    catch (error) {
-        if (axios.isAxiosError(error)) {
+    catch (error) 
+    {
+        if (axios.isAxiosError(error)) 
+        {
             console.log("error message: ", error.message);
             return error.message;
         }
-        else {
+        else
+        {
         console.log("unexpected error: ", error);
         return "An unexpected error has occured";
         }
